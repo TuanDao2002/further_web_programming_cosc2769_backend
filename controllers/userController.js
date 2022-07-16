648217e95068da0031e94133d273b020b9f2042b
kaisergrimlock <s3891987@rmit.edu.vn>
2022-07-16 15:47:22 +0700

    delete and findall
    Draft

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,18 +2,17 @@ const User = require("../models/User")
 const CustomError = require("../errors");
 
 const getAllUsers = async (req, res) => {
-
+    return User.find()
 }
 
 const deleteUser = async (req, res) => {
     const { params: { id: userId } } = req;
-    const user = await User.findOne({ _id: userId })
+    const user = await User.findOne({ _id: userId });
     if (!user) {
-        throw new CustomError.BadRequestError(
-
-        )
-    }
-
+        throw new CustomError.BadRequestError();
+    }else{
+        User.deleteOne({ _id: userId});
+    };
 }
 
 module.exports = {
