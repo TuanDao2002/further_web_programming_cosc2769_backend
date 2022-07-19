4ce8eb207e61dc69d134ba87b7ccd81b1b37e978
s3878694 <s3878694@rmit.edu.vn>
2022-07-19 20:12:59 +0700

    Models for Room and Message

--- models/Message.js
+++ models/Message.js
@@ -2,3 +2,30 @@
 // content -> type: String
 // userID -> type: mongoose.Schema.ObjectId, ref: "User
 // timestamp: true
+
+const mongoose = require("mongoose")
+
+const MessageSchema = new mongoose.Schema(
+    {
+       roomId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Room",
+        require: true
+       },
+       
+       content: {
+        type: String,
+        require: true
+       },
+
+       userId: {
+        type: mongoose.Types.ObjectId,
+        reqire: true
+       }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Message", MessageSchema)
--- models/Room.js
+++ models/Room.js
@@ -1,2 +1,15 @@
 // _id of Room is the id of Socket.io
 // users -> [mongoose.Schema.ObjectId], ref: "User"
+const mongoose = require("mongoose");
+
+const RoomSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: [mongoose.Types.ObjectId],
+            ref: "User",
+            require: true
+        }
+    }
+);
+
+module.exports = mongoose.model("Room", RoomSchema);
