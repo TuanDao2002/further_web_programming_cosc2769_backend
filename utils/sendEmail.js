5b12248097ac75267a081623e6936a604d4c30e9
TuanDao2002 <tuan09092002@gmail.com>
2022-09-08 18:06:00 +0700

    update gmail config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,22 +1,35 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, html) => {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com", // hostname
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.MAIL,
-            pass: process.env.PASS,
-        },
-    });
+	// const transporter = nodemailer.createTransport({
+	//     host: "smtp.gmail.com", // hostname
+	//     port: 465,
+	//     auth: {
+	//         user: process.env.MAIL,
+	//         pass: process.env.PASS,
+	//     },
+	// });
 
-    return transporter.sendMail({
-        from: process.env.MAIL,
-        to: email,
-        subject,
-        html,
-    });
+	const transporter = nodemailer.createTransport({
+		host: "smtp.gmail.com",
+		secureConnection: false,
+		port: 587,
+		tls: {
+			ciphers: "SSLv3",
+		},
+		requireTLS: true,
+		auth: {
+			user: process.env.MAIL,
+			pass: process.env.PASS,
+		},
+	});
+
+	return transporter.sendMail({
+		from: process.env.MAIL,
+		to: email,
+		subject,
+		html,
+	});
 };
 
 module.exports = sendEmail;
