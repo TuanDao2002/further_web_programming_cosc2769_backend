c7afec58c5aa40aeeea513a6a442d2f0a6391a8f
quangan186 <61352012+quangan186@users.noreply.github.com>
2022-09-09 23:45:36 +0700

    change email reset password's link

--- utils/sendResetPasswordEmail.js
+++ utils/sendResetPasswordEmail.js
@@ -7,7 +7,7 @@ const sendResetPasswordEmail = async (
     origin,
     minutesToExpire
 ) => {
-    const resetURL = `${origin}/forgot-password?token=${verificationToken}`;
+    const resetURL = `${origin}/reset-password?token=${verificationToken}`;
     const message = `<center><h3>There is an attempt to reset password at ${browser} browser.</h3></center>
                     <center><h3>If this was you, clicking on the following link (it will expire after ${minutesToExpire} minutes): <a href="${resetURL}">Reset password</a></h3><center>`;
     return sendEmail(email, "Reset password", message);
