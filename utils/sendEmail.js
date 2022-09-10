8b2d73dae623dce081fe98a568b1c91ecad6dc43
TuanDao2002 <s3877347@rmit.edu.vn>
2022-09-10 09:41:23 +0700

    change gmail config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,27 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, html) => {
-	// const transporter = nodemailer.createTransport({
-	//     host: "smtp.gmail.com", // hostname
-	//     port: 465,
-	//     auth: {
-	//         user: process.env.MAIL,
-	//         pass: process.env.PASS,
-	//     },
-	// });
-
 	const transporter = nodemailer.createTransport({
-		host: "smtp.gmail.com",
-		secureConnection: false,
-		port: 587,
-		tls: {
-			ciphers: "SSLv3",
-		},
-		requireTLS: true,
-		auth: {
-			user: process.env.MAIL,
-			pass: process.env.PASS,
-		},
+	    host: "smtp.gmail.com", // hostname
+	    port: 465,
+	    auth: {
+	        user: process.env.MAIL,
+	        pass: process.env.PASS,
+	    },
 	});
 
 	return transporter.sendMail({
